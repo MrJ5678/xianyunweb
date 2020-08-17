7317ed4faca02e87a93cc48e27bb6c19433a3b2d
hhhhhq <Mr.j@tom.com>
2020-08-17 21:23:04 +0800

    before-add-vuex-persistedstate

---
+++ store/user.js
@@ -0,0 +1,15 @@
+export const state = () => ({
+  // 采用接口返回的数据结构
+  userInfo: {
+    token: "",
+    user: {},
+  }
+});
+
+export const mutations = {
+  setUserInfo(state, data) {
+    state.userInfo = data;
+  }
+};
+
+export const actions = {};
